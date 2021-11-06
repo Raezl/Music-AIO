59a3c47a57ea65e67d3cd24340834910a01c1b4b
Yasiru Lakruwan <yasiru.2015090@iit.ac.lk>
2021-11-06 22:25:31 +0530

    Added error handling for user
    Error handling for for when logging  in and registering

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,71 +1,57 @@
 const USER = require('../models/usersModel');
 const userValidation = require('../validations/userValidation');
 const HASH = require('../hash');
+const jwt = require('jsonwebtoken')
 
-
-function findUserByEmail(data) {
-    return new Promise((resolve, reject) => {
-        const user = USER.findOne({ email: data })
-        if (user) {
-            resolve(user);
-        }
-        reject(new Error('not found'));
-    })
+//Find user detail by email
+findUserByEmail = async (data) => {
+    return USER.findOne({ email: data });
 }
 
 
-//Find username
-exports.login = function (req, res) {
-    findUserByEmail(req.body.email).then(user => {
-        if (user != null) {
-            console.log(user._id);
-            HASH.genHash(req.body.password).then(data => {
-                console.log(data);
-                HASH.verifyPassword(user.password, data).then(verified => {
-                    if (verified === true) {
-                        console.log(verified);
-                        const token = jwt.sign({ _id: user._id }, process.env.SECRET_TOKEN);
-                        res.header('auth-token', token).send('Authorised');
-                    }
-                })
-            //Handle errors if password not matched
-            }).catch(err =>{
-                res.json({message: err});
-            });
+//Check for user and return jwt token
+exports.login = async function (req, res) {
+    try {
+        const user = await findUserByEmail(req.body.email);
+        if (user === null) throw 'Email not found';
+
+        const hashedpassword = await HASH.genHash(req.body.password);
+
+        if (HASH.verifyPassword(req.body.password, hashedpassword)) {
+            //create jwt token 
+            const token = jwt.sign();
+            res.status(200).json({ message: 'Authenticated' });
         }
-    //Handle errors if user email is not in db
-    }).catch(err =>{
-        res.status(200).json({message: err});
-    })
+    } catch (err) {
+        res.status(200).json({ message: err });
+    }
+
 };
 
 //Add new user 
-exports.register = function (req, res) {
+exports.register = async function (req, res) {
+    try {
+        const { error } = userValidation.registerValidation(req.body);
+        if (error) throw error.message;
 
-    const { error } = userValidation.registerValidation(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+        const user = await findUserByEmail(req.body.email);
+        if (user) throw 'Email exits';
 
-    //check for existing user email
-    USER.findOne({ email: req.body.email })
-        .then(email => {
-            if (email) return res.status(409).send('Email already exist');
-
-            //Hash password 
-            HASH.genHash(req.body.password).then(hash => {
-                //add new user to db
-                const user = new USER({
-                    email: req.body.email,
-                    password: hash
-                });
-                user.save()
-                    .then(
-                        res.status(200).json({ message: 'Success' })
-                    )
-                    .catch(err => {
-                        res.status(200).json({ messege: err });
-                    });
-            });
+        //Hash password  
+        const hash = await HASH.genHash(req.body.password);
+        //create new user 
+        const newUser = new USER({
+            email: req.body.email,
+            password: hash
         });
-};
+
+        //add new user to db
+        const registred = await newUser.save();
+        if (registred) return res.status(200).json({ message: 'Authorised' });
+
+    } catch (err) {
+        res.status(200).json({ message: err });
+    }
+}
 
 
